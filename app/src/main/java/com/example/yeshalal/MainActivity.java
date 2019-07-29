ae1e0bfb7cfb357c44f1ca9ba560d3b9d11d0b72
Khalid Ahmad <khalid.ahmad063@gmail.com>
2019-07-29 18:20:51 +0600

    Add SplashScreen

--- app/src/main/java/com/example/yeshalal/MainActivity.java
+++ app/src/main/java/com/example/yeshalal/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.yeshalal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+
+    // for Splash Screen
+    private static int SPLASH_TIME_OUT = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
         setContentView (R.layout.activity_main);
+
+        //S. for SplashScreen
+        new Handler ().postDelayed (new Runnable () {
+            @Override
+            public void run() {
+                Intent i = new Intent (MainActivity.this, HomeActivity.class);
+                startActivity (i);
+                finish ();
+            }
+        },SPLASH_TIME_OUT); //E. for SplashScreen
+
+
     }
 }
