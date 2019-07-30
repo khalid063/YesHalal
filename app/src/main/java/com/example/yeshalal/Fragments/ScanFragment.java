e65564ac12d9fbc6b9282c4c7ec22791a759dd74
Khalid Ahmad <khalid.ahmad063@gmail.com>
2019-07-30 11:30:29 +0600

    Scan Page Design Complete

--- app/src/main/java/com/example/yeshalal/Fragments/ScanFragment.java
+++ app/src/main/java/com/example/yeshalal/Fragments/ScanFragment.java
@@ -9,11 +9,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.example.yeshalal.R;
 
 
 public class ScanFragment extends Fragment {
+
+    private LinearLayout linearLayoutToolsButton;
 
 
     @Override
@@ -22,6 +25,15 @@ public class ScanFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate (R.layout.fragment_scan, container, false);
 
+        linearLayoutToolsButton = view.findViewById (R.id.home_tools_button_id);
+        linearLayoutToolsButton.setOnClickListener (new View.OnClickListener () {
+            @Override
+            public void onClick(View view) {
+
+                getFragmentManager ().beginTransaction ().replace (R.id.fragment_container, new ToolsFragment ()).commit ();
+
+            }
+        });
 
         return view;
     }
--- app/src/main/java/com/example/yeshalal/Fragments/ToolsFragment.java
+++ app/src/main/java/com/example/yeshalal/Fragments/ToolsFragment.java
@@ -9,11 +9,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.example.yeshalal.R;
 
 
 public class ToolsFragment extends Fragment {
+
+    private LinearLayout linearLayoutScanButton;
 
 
     @Override
@@ -22,6 +25,15 @@ public class ToolsFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate (R.layout.fragment_tools, container, false);
 
+        linearLayoutScanButton = view.findViewById (R.id.tools_scan_button_id);
+        linearLayoutScanButton.setOnClickListener (new View.OnClickListener () {
+            @Override
+            public void onClick(View view) {
+
+                getFragmentManager ().beginTransaction ().replace (R.id.fragment_container, new ScanFragment ()).commit ();
+
+            }
+        });
 
         return view;
     }
