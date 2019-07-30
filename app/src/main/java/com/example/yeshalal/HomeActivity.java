c632b9831ff4e219fbb2c7a454266c115ba5d50f
Khalid Ahmad <khalid.ahmad063@gmail.com>
2019-07-30 10:09:46 +0600

    logo icon change

--- app/src/main/java/com/example/yeshalal/HomeActivity.java
+++ app/src/main/java/com/example/yeshalal/HomeActivity.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.yeshalal.Fragments.ScanFragment;
+
 public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
         setContentView (R.layout.activity_home);
+
+        // For changing the fragment
+        getSupportFragmentManager ().beginTransaction ().replace (R.id.fragment_container, new ScanFragment ()).commit ();
+
+
     }
 }
