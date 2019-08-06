ac3ea5ff94b485ec1ff4f4e16c8e73d14f82992b
Khalid Ahmad <khalid.ahmad063@gmail.com>
2019-08-06 18:18:16 +0600

    working on drawer nav manu

--- app/src/main/java/com/example/yeshalal/Fragments/ScanFragment.java
+++ app/src/main/java/com/example/yeshalal/Fragments/ScanFragment.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -16,7 +18,9 @@ import com.example.yeshalal.R;
 
 public class ScanFragment extends Fragment {
 
-    private LinearLayout linearLayoutToolsButton;
+    // outside of OnCreat
+    LinearLayout linearLayoutToolsButton, linearLayoutDrawarButtton;
+    DrawerLayout drawerLayout;
 
 
     @Override
@@ -26,12 +30,25 @@ public class ScanFragment extends Fragment {
         View view = inflater.inflate (R.layout.fragment_scan, container, false);
 
         linearLayoutToolsButton = view.findViewById (R.id.home_tools_button_id);
+        // for drawer layout
+        linearLayoutDrawarButtton = view.findViewById (R.id.home_drawer_menu_button_id);
+        drawerLayout = view.findViewById (R.id.drawar_layout);
+
+
         linearLayoutToolsButton.setOnClickListener (new View.OnClickListener () {
             @Override
             public void onClick(View view) {
 
                 getFragmentManager ().beginTransaction ().replace (R.id.fragment_container, new ToolsFragment ()).commit ();
 
+            }
+        });
+
+        // set Listener
+        linearLayoutDrawarButtton.setOnClickListener (new View.OnClickListener () {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.openDrawer(GravityCompat.START);
             }
         });
 
